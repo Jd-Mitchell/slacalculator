33206fd5cf6e3e0e9644a246e5dc088a649da817
JD Mitchell <jdpmitchell@gmail.com>
2026-04-27 12:48:33 +1000

    new build number

--- src/ui/Version.js
+++ src/ui/Version.js
@@ -3,8 +3,8 @@ import { UICreator } from './UICreator.js'
 class Version {
 
     static state = {
-        version: '2.0.0',
-        build: 2,
+        version: '2.0.1',
+        build: 3,
         isDev: true
     }
 
