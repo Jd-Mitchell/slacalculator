f1ad1527b0e930c3283a21017554d504fe4af70a
JD Mitchell <jdpmitchell@gmail.com>
2026-05-11 13:06:12 +1000

    fix: failsafe check to use difference if already calculated
    Added a check to add the difference to the opening time if
    timeKeeper.isCalculated is true. This ensures that when the handleOutsideHours check has already
    calculated the difference, the correct SLA is set when the failsafe has tripped instead of
    using the non-calculated check that already exists.

--- src/ui/Version.js
+++ src/ui/Version.js
@@ -3,8 +3,8 @@ import { UICreator } from './UICreator.js'
 class Version {
 
     static state = {
-        version: '2.0.3',
-        build: 7,
+        version: '2.0.4',
+        build: 8,
         isDev: true
     }
 
